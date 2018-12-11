bdb3f48c26746c0fbff66dd252b611819dc08d56
NicoV <nicolas@wildcodeschool.fr>
2018-12-11 16:44:26 +0100

    Entity Type Simple

---
+++ src/Form/EventType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('users', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'firstname',
+                'multiple' => false,
+                'expanded' => false])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
